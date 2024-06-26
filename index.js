fce2434a83d336b9891e66336596f0d8bf183059
abudhosamuel <abudhosamuel@gmail.com>
2024-06-26 22:27:33 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// Function expression called divide
+const divide = (a, b) => {
+    return a / b;
+  };
+  
+  // Arrow function called square
+  const square = x => {
+    return x * x;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Testing the functions manually
+  console.log(divide(2000, 100)); // Should output: 20
+  console.log(square(2)); // Should output: 4
+  console.log(add(3, 4)); // Should output: 7
+  
